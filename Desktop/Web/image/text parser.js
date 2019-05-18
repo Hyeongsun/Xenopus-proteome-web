763c5ee90a5d58203d34982964271dfec2fa946f
Hyeongsun <jhs9301@unist.ac.kr>
2019-05-18 21:51:57 +0900

    image

---
+++ Desktop/Web/image/text parser.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+var text = fs.readFileSync("./proteinGroups.txt").toString().split("\n");
+// General Code
+var protein = read("proteinGroups.txt").split("\t");
+for (var i = 0; i < protein.length; ++i) {
+          protein[i] = protein[i].trim();
+       }
+for (var i = 0; i < names.length; ++i) {
+var name = protein[i].split("\t");
+}}
